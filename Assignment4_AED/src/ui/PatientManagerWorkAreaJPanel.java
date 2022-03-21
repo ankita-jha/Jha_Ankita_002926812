52e14f147538b83d580818730647900607d696ba
ankita_jha <jha.ank@northeastern.edu>
2022-03-20 21:17:41 -0400

    changes

--- Assignment4_AED/src/ui/PatientManagerWorkAreaJPanel.java
+++ Assignment4_AED/src/ui/PatientManagerWorkAreaJPanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package userInteface;
+package ui;
 
 import business.PersonDirectory;
 import java.awt.CardLayout;
